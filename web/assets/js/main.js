f733335a72f6a5cf4cf2f6679de03f25ed183e3a
Jan Hendrickx <jan@janhendrickx.be>
2025-11-03 11:11:40 +0100

    working horizontal masonry, local js

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -15,31 +15,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     imagesLoaded( elem ).on( 'always', function() {
         pckry.layout();
-
-    var filterGroup = document.querySelector('.category-nav'); // Selecteer de navigatiecontainer
         
-        filterGroup.addEventListener( 'click', function( event ) {
-            
-            // 1. Controleer of er op een 'a' tag met de class 'nav-link' is geklikt
-            if ( !event.target.matches('.nav-link') ) {
-                return;
-            }
-            
-            // 2. VOORKOM dat de pagina naar de top springt
-            event.preventDefault(); 
-            
-            var filterValue = event.target.getAttribute('data-filter');
-            
-            // 3. Pas het filter toe op het Packery/Isotope object
-            pckry.filter( filterValue );
-            
-            // 4. Stijl de actieve link (is-checked class)
-            var links = filterGroup.querySelectorAll('.nav-link');
-            for ( var i=0; i < links.length; i++ ) {
-                links[i].classList.remove('is-checked');
-            }
-            event.target.classList.add('is-checked');
-        });
-
     });
 });
